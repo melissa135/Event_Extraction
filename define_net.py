00dfb4052fbf207fb4072572128ee8e6b02afd01
melissa135 <1071560244@qq.com>
2019-01-25 20:09:18 +0800

    Update define_net.py

--- define_net.py
+++ define_net.py
@@ -68,57 +68,6 @@ class BiLSTM(nn.Module):
                 Variable(torch.zeros(num_layers*self.num_hidden, batch, self.hidden_size)).cuda())
 
 
-class NER_simple(nn.Module):
-    def __init__(self, event_dim):
-        super(NER_simple, self).__init__()
-        self.linear = nn.Linear(256*2,event_dim)
-        self.softmax = nn.LogSoftmax(dim=1)
-
-    def forward(self, inputs):
-        inputs = inputs.view(-1,256*2)
-        inputs = self.linear(inputs)
-        inputs = self.softmax(inputs)
-        return inputs
-
-
-class NER_old(nn.Module):
-    def __init__(self, event_dim):
-        super(NER_old, self).__init__()
-        self.event_dim = event_dim
-        self.BiLSTM_hidden_size = 2*256 # must be same as previous
-        self.NER_hidden_size = event_dim
-        self.num_hidden = 1
-        self.event_dim = event_dim
-
-        self.lstm = nn.LSTMCell(self.BiLSTM_hidden_size+self.event_dim, \
-                                self.NER_hidden_size, self.num_hidden)
-        self.linear = nn.Linear(self.NER_hidden_size, self.event_dim)
-        self.softmax = nn.LogSoftmax(dim=1)
-
-    def forward(self, bilstm_output, ner_softmax, ner_hidden):
-
-        ner_output = []
-        hidden,cell_state = ner_hidden
-
-        for i in range(0,bilstm_output.size()[0]) :
-            inputs = torch.cat((bilstm_output[i], ner_softmax), 1) # N*(BiLSTM_hidden+linear_result)
-            hidden,cell_state = self.lstm(inputs,(hidden,cell_state)) # two N*self.NER_hidden_size
-            hidden = self.linear(hidden) # N*self.NER_hidden_size -> N*event_dim
-            ner_softmax = self.softmax(hidden) # N*event_dim
-            tmp = ner_softmax.view(-1,self.event_dim)
-            ner_output.append(tmp)
-
-        ner_output = torch.cat(ner_output,0) # L*event_dim
-        return ner_output
-
-    def initHidden(self,batch=1):
-        return (Variable(torch.zeros(batch, self.NER_hidden_size)),
-                Variable(torch.zeros(batch, self.NER_hidden_size)))
-
-    def initSoftmax(self,batch=1):
-        return Variable(torch.zeros(batch, self.event_dim))
-
-
 class Trigger_Recognition(nn.Module):
     def __init__(self, event_dim):
         super(Trigger_Recognition, self).__init__()
@@ -203,6 +152,8 @@ class Relation_Classification(nn.Module):
 
         self.conv_3 = nn.Conv1d(self.BiLSTM_hidden_size+2*self.ner_embedding,self.conv_out_dim,3,1,1)
         self.conv_3r = nn.Conv1d(self.BiLSTM_hidden_size+2*self.ner_embedding,self.conv_out_dim,3,1,1)
+        #self.conv_5 = nn.Conv1d(self.BiLSTM_hidden_size+2*self.ner_embedding,self.conv_out_dim,5,1,2)
+        #self.conv_5r = nn.Conv1d(self.BiLSTM_hidden_size+2*self.ner_embedding,self.conv_out_dim,5,1,2)
 
         self.linear = nn.Linear(6*self.ner_embedding+3*self.BiLSTM_hidden_size+self.position_dim+self.conv_out_dim, tmp_dim)
         self.linear2 = nn.Linear(tmp_dim, self.relation_dim)
@@ -233,8 +184,10 @@ class Relation_Classification(nn.Module):
         #middle = middle[:,0:self.BiLSTM_hidden_size,:]
         if not reverse_flag :
             middle_c = self.conv_3(middle)
+            # middle_c5 = self.conv_5(middle)
         else :
             middle_c = self.conv_3r(middle)
+            # middle_c5 = self.conv_5r(middle)
         middle_c = self.max_pooling(middle_c)
 
         x = torch.cat((src,middle_p,middle_c,dst,pe_middle),1) # N * 3 BiLSTM_hidden_size * 1
@@ -245,208 +198,3 @@ class Relation_Classification(nn.Module):
         x = self.softmax(x)# N * ( probility of each relation )
 
         return x
-'''
-
-class Relation_Classification(nn.Module):
-    def __init__(self, relation_dim):
-        super(Relation_Classification, self).__init__()
-        self.BiLSTM_hidden_size = 2*128 # must be same as previous
-        self.relation_dim = relation_dim
-        self.ner_embedding = 16
-        tmp_dim = 128
-        self.context_relation_dim = 64
-
-        self.position_dim = 5
-        self.max_position = 16
-        self.position_embedding = nn.Embedding(2*self.max_position+1, self.position_dim)
-        self.empty_embedding = nn.Embedding(1, self.BiLSTM_hidden_size)
-        #self.context_relation_embedding = nn.Embedding(self.relation_dim, self.context_relation_dim)
-
-        self.pooling = nn.AdaptiveMaxPool1d(1)
-
-        self.conv_out_dim = 128
-        self.conv_3 = nn.Conv1d(self.BiLSTM_hidden_size,self.conv_out_dim,3,1,1)
-        self.conv_3r = nn.Conv1d(self.BiLSTM_hidden_size,self.conv_out_dim,3,1,1)
-
-        self.linear = nn.Linear(2*self.ner_embedding+self.BiLSTM_hidden_size+self.conv_out_dim+self.position_dim, tmp_dim)
-        self.linear2 = nn.Linear(tmp_dim, self.relation_dim)
-
-        #self.conv_5 = nn.Conv1d(self.BiLSTM_hidden_size,self.conv_out_dim,5,1,2)
-        #self.conv_5r = nn.Conv1d(self.BiLSTM_hidden_size,self.conv_out_dim,5,1,2)
-
-        self.softmax = nn.LogSoftmax(dim=1)
-        self.dropout = nn.Dropout(p=0.2)
-
-    def forward(self, src_event,src,middle,dst,dst_event,reverse_flag,middle_flag):
-        tensor = torch.LongTensor(1,1).zero_()
-        if middle_flag:
-            tensor[0][0] = 0
-        else :
-            tensor[0][0] = middle.size()[0]
-        if tensor[0][0] > self.max_position :
-            tensor[0][0] = self.max_position
-        if reverse_flag :
-            tensor[0][0] = - tensor[0][0]
-        tensor[0][0] = tensor[0][0] + self.max_position
-        pe = self.position_embedding(Variable(tensor).cuda())
-        pe = pe.view(1,-1,1)
-
-        src_event = src_event.permute(1,2,0)
-        src_event = self.pooling(src_event)
-        s_m_d = torch.cat((src,middle,dst),0).permute(1,2,0)  # (L1+L2+L3)*N*BiLSTM_hidden_size -> N*BiLSTM_hidden_size*(L1+L2+L3)
-        s_m_d_1 = self.pooling(s_m_d)
-        dst_event = dst_event.permute(1,2,0)
-        dst_event = self.pooling(dst_event)
-
-        if not reverse_flag :
-            s_m_d_2 = self.conv_3(s_m_d) ###
-            s_m_d_2 = self.pooling(s_m_d_2)
-        else :
-            s_m_d_2 = self.conv_3r(s_m_d) ###
-            s_m_d_2 = self.pooling(s_m_d_2)
-
-        x = torch.cat((src_event,s_m_d_1,s_m_d_2,dst_event,pe),1) # N * 3 BiLSTM_hidden_size * 1
-        x = x.view(1,-1) # N(=1) * 3 BiLSTM_hidden_size
-        #x = self.layer_norm(x)
-        x = self.dropout(x)
-        x = F.leaky_relu(self.linear(x)) # N*relation_dim
-        x = self.linear2(x)
-        x = self.softmax(x)# N * ( probility of each relation )
-
-        return x
-'''
-
-class RC_cnn(nn.Module):
-    def __init__(self, relation_dim):
-        super(RC_cnn, self).__init__()
-        self.BiLSTM_hidden_size = 2*256 # must be same as previous
-        self.relation_dim = relation_dim
-        self.ner_embedding = 16
-        self.conv_out_dim = 128
-
-        self.max_position = 32
-        self.position_dim = 5
-
-        self.position_embedding_src = nn.Embedding(2*self.max_position+1, self.position_dim)
-        self.position_embedding_dst = nn.Embedding(2*self.max_position+1, self.position_dim)
-        self.conv_3 = nn.Conv1d(self.BiLSTM_hidden_size+self.ner_embedding+2*self.position_dim, \
-                                self.conv_out_dim,3,1,1)
-        self.pooling = nn.AdaptiveMaxPool1d(1)
-        self.linear = nn.Linear(3*self.conv_out_dim, 128)
-        self.linear2 = nn.Linear(128, self.relation_dim)
-        self.softmax = nn.LogSoftmax(dim=1)
-        self.dropout = nn.Dropout(p=0.1)
-
-    def forward(self, bilstm_output,ner_emb,src_range,dst_range):
-
-        length = bilstm_output.size()[0]  # L
-        tensor_a = torch.LongTensor(1,length).zero_() # N*L
-        tensor_b = torch.LongTensor(1,length).zero_() # N*L
-
-        for i in range(0,src_range[0]):
-            if src_range[0]-i < self.max_position :
-                tensor_a[0][i] = i - src_range[0]
-            else :
-                tensor_a[0][i] = -self.max_position
-
-        for i in range(src_range[1]+1,length):
-            if i-src_range[1] < self.max_position :
-                tensor_a[0][i] = i - src_range[1]
-            else :
-                tensor_a[0][i] = self.max_position
-
-        for i in range(0,dst_range[0]):
-            if dst_range[0]-i < self.max_position :
-                tensor_b[0][i] = i - dst_range[0]
-            else :
-                tensor_b[0][i] = -self.max_position
-
-        for i in range(dst_range[1]+1,length):
-            if i-dst_range[1] < self.max_position :
-                tensor_b[0][i] = i - dst_range[1]
-            else :
-                tensor_b[0][i] = self.max_position
-
-        for i in range(0,length):
-            tensor_a[0][i] = tensor_a[0][i] + self.max_position
-            tensor_b[0][i] = tensor_b[0][i] + self.max_position
-
-        position_index_a = Variable(tensor_a)
-        position_index_b = Variable(tensor_b)
-        position_output_a = self.position_embedding_src(position_index_a) # N*L*position_dim
-        position_output_b = self.position_embedding_dst(position_index_b) # N*L*position_dim
-        position_output = torch.cat((position_output_a,position_output_b),2) # N*L*(2*position_dim)
-        position_output = position_output.permute(1,0,2) # L*N*(2*position_dim)
-
-        input_ = torch.cat((bilstm_output,ner_emb,position_output),2) # L*N*(BiLSTM_hidden_size+ner_embedding+2*position_dim)
-        input_ = input_.permute(1,2,0) # N*(BiLSTM_hidden_size+ner_embedding+2*position_dim)*L
-
-        if src_range[0] < dst_range[0] :
-            split1,split2 = src_range,dst_range
-        else :
-            split1,split2 = dst_range,src_range
-
-        input_a = input_[:,:,0:split1[1]+1]
-        input_b = input_[:,:,split1[0]:split2[1]+1]
-        input_c = input_[:,:,split2[0]:]
-
-        input_a = self.conv_3(input_a) # N * conv_out_dim * L
-        input_b = self.conv_3(input_b) # N * conv_out_dim * L
-        input_c = self.conv_3(input_c) # N * conv_out_dim * L
-
-        input_a = self.pooling(input_a) # N * conv_out_dim * 1
-        input_b = self.pooling(input_b) # N * conv_out_dim * 1
-        input_c = self.pooling(input_c) # N * conv_out_dim * 1
-
-        input_a = input_a.view(1,-1) # N * conv_out_dim
-        input_b = input_b.view(1,-1) # N * conv_out_dim
-        input_c = input_c.view(1,-1) # N * conv_out_dim
-
-        x = torch.cat((input_a,input_b,input_c),1) # N*(3*conv_out_dim)
-        x = self.dropout(x)
-        x = F.relu(self.linear(x)) # N*relation_dim
-        x = self.linear2(x)
-        x = self.softmax(x)# N * ( probility of each relation )
-        return x
-
-
-class RC_lstm(nn.Module):
-    def __init__(self, relation_dim, marker_type=3):
-        super(RC_lstm, self).__init__()
-        self.BiLSTM_hidden_size = 2*256 # must be same as previous
-        self.relation_dim = relation_dim
-        self.ner_embedding = 16
-        self.marker_dim = 5
-        self.hidden_size = 192
-
-        self.marker_embedding = nn.Embedding(marker_type, self.marker_dim)
-        self.lstm = nn.LSTM(self.BiLSTM_hidden_size+self.ner_embedding+self.marker_dim, self.hidden_size)
-        self.linear = nn.Linear(self.hidden_size, relation_dim)
-        self.softmax = nn.LogSoftmax(dim=1)
-        self.dropout = nn.Dropout(p=0.1)
-
-    def forward(self, bilstm_output,ner_emb,src_range,dst_range,hidden_):
-
-        length = bilstm_output.size()[0]  # L
-        tensor = torch.LongTensor(1,length).zero_() # N*L
-        for i in range(src_range[0],src_range[1]+1):
-            tensor[0][i] = 1
-        for i in range(dst_range[0],dst_range[1]+1):
-            tensor[0][i] = 2
-
-        marker_index = Variable(tensor)
-        marker_output = self.marker_embedding(marker_index) # N*L*marker_dim
-        marker_output = marker_output.permute(1,0,2) # L*N*marker_dim
-
-        input_ = torch.cat((bilstm_output,ner_emb,marker_output),2) # L*N*(BiLSTM_hidden_size+ner_embedding+marker_dim)
-        output, (hidden,c_state) = self.lstm(input_, hidden_) # L*N*self.hidden_size
-        output = hidden.view(1,-1) # N*self.hidden_size
-        #output = [[1 for i in range(0,self.hidden_size)]]
-        #output = Variable(torch.FloatTensor(output))
-        output = self.linear(output)
-        output = self.softmax(output) # N*relation_dim
-        return output, hidden
-
-    def initHidden(self,num_layers=1,batch=1):
-        return (Variable(torch.zeros(num_layers, batch, self.hidden_size)),
-                Variable(torch.zeros(num_layers, batch, self.hidden_size)))
